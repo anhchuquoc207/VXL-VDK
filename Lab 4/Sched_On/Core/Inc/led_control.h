829874fea09e065359c6af72fcb5360477ef7b8f
anhchuquoc207 <anh.chuquoc207@hcmut.edu.vn>
2025-10-26 10:54:38 +0700

    Add files via upload

---
+++ Lab 4/Sched_On/Core/Inc/led_control.h
@@ -0,0 +1,11 @@
+#ifndef INC_LED_CONTROL_H_
+#define INC_LED_CONTROL_H_
+
+void setUp(void);
+void blinky0();
+void blinky1();
+void blinky2();
+void blinky3();
+void blinky4();
+
+#endif /* INC_LED_CONTROL_H_ */
